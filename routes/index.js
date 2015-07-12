31145dd3d8f017bc0b526da1bf852587e1e17aaf
db7ech <db7ech@gmail.com>
2015-07-12 10:40:28 +0200

    version definitiva
    Actualizacion creditos,author, y author.jpg

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
+var creditsController = require('../controllers/credits_controller');
+
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+/* GET credits page. */
+
+router.get('/author', creditsController.credits);
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer',   quizController.answer);
 
